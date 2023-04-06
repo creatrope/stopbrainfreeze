df45ebe2d121604c0eb18cd583c318fc55e363a9
creatrope <sendhb@gmail.com>
2023-04-06 00:22:12 -0700

    Delete aotd.js

--- assets/js/aotd.js
+++
@@ -1,9 +0,0 @@
-fetch('{{ site.baseurl }}/data/affirmations.json')
-  .then(response => response.json())
-  .then(data => {
-    const index = Math.floor(Math.random() * data.affirmations.length);
-    const affirmation = data.affirmations[index];
-    const affirmationElement = document.getElementById('affirmation');
-    affirmationElement.textContent = affirmation;
-  })
-  .catch(error => console.error(error));
