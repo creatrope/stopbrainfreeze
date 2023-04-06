32b7b328b0de2b8b8a99ff05c699403ecbcb496c
creatrope <sendhb@gmail.com>
2023-04-05 23:18:28 -0700

    Update aotd.js

--- assets/js/aotd.js
+++ assets/js/aotd.js
@@ -1,16 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let response = fetch('/assets/affirmations.json')
-    .then(res => res.text())
-    .then(data => {
-        let json = JSON.parse(data);
-        let date = new Date(); 
-        let doy = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
-        let idx = doy % json.length;
-        let quote = json[idx];
-        document.getElementById('quote').innerHTML = '<blockquote class="not-prose"><p>' + quote.text + '</p><p><em>- ' + quote.author + '</em></p></blockquote>';
-    })
-    .catch(res => {
-        console.error('Could not fetch affirmations.json'); 
-        console.error(res); 
-    });
-});
+fetch('{{ site.baseurl }}/data/affirmations.json')
+  .then(response => response.json())
+  .then(data => {
+    const index = Math.floor(Math.random() * data.affirmations.length);
+    const affirmation = data.affirmations[index];
+    const affirmationElement = document.getElementById('affirmation');
+    affirmationElement.textContent = affirmation;
+  })
+  .catch(error => console.error(error));
