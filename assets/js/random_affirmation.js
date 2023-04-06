d33f0a08810dc0b8148c65ac4cb008c32c6f5d0f
creatrope <sendhb@gmail.com>
2023-04-06 00:18:26 -0700

    Create random_affirmation.js

---
+++ assets/js/random_affirmation.js
@@ -0,0 +1,6 @@
+---
+---
+const affirmations = {% assign affirmations = site.data.affirmations | join: ',' %};
+const affirmationsArray = affirmations.split(',');
+const randomIndex = Math.floor(Math.random() * affirmationsArray.length);
+document.getElementById("affirmation").innerHTML = affirmationsArray[randomIndex];
